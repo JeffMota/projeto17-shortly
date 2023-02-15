75d02c127e99acf8ec44f4ea79685db30b7bebd0
JeffMota <jmota906@gmail.com>
2023-02-15 11:35:48 -0300

    Initial commit

---
+++ src/controller/Auth.controller.js
@@ -0,0 +1,11 @@
+
+
+//Cadastro de usuário
+export async function signup(req, res) {
+    res.send("Rota de cadastro")
+}
+
+//Login
+export async function signin(req, res) {
+    res.send("Rota de login")
+}
