322ef7ee842870fe7e397c0bf17797873268ee2f
JeffMota <jmota906@gmail.com>
2023-03-01 09:20:36 -0300

    feat: get url by id

--- src/controller/Url.controller.js
+++ src/controller/Url.controller.js
@@ -17,3 +17,17 @@ export async function shortUrl(req, res) {
     }
 
 }
+
+export async function getUrlById(req, res) {
+    const id = req.params.id
+
+    try {
+
+        const { rows } = await db.query('SELECT urls.id, urls."shortUrl", urls.url FROM urls;')
+
+        res.send({ id: rows[0].id, shortUrl: rows[0].shortUrl, url: rows[0].url })
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
--- src/routes/Urls.routes.js
+++ src/routes/Urls.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express"
-import { shortUrl } from "../controller/Url.controller.js"
+import { getUrlById, shortUrl } from "../controller/Url.controller.js"
 import validateSchema from "../middleware/ValidateSchemas.js"
 import validateToken from "../middleware/ValidateToken.js"
 import { shortUrlSchema } from "../schemas/Urls.schema.js"
 
 const urlRouter = Router()
 
-urlRouter.post('/urls/shorten', validateToken(), validateSchema(shortUrlSchema), shortUrl)
+urlRouter.get('/urls/:id', getUrlById)
+
+urlRouter.use(validateToken())
+urlRouter.post('/urls/shorten', validateSchema(shortUrlSchema), shortUrl)
 
 export default urlRouter
